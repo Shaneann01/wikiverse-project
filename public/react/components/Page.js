9d0d517394f78cf2921714dabdc7710a6af7d5d6
Shaneann01 <shaneann.ahtack@vodafone.com>
2023-11-19 16:06:54 +0000

    Now have the page title linked to the article

--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Article } from "./Article";
+import { Link } from "react-router-dom";
 
 export const Page = (props) => {
   return (
     <>
       <h3>{props.page.title}</h3>
-      <Article />
+      <Link to="/Article">{props.page.title}</Link>
     </>
   );
 };
